ec6e7c3fde607f2d4ba931630a1b1be9992728c5
dereklwh <derek141772@gmail.com>
2025-10-23 08:31:42 -0700

    init commit

---
+++ server.py
@@ -0,0 +1,11 @@
+print('Hello World')
+
+# Requirements:
+# Respond to requests with following responses:
+
+# Code	Message
+# 200	OK
+# 304	Not Modified
+# 403	Forbidden
+# 404	Not Found
+# 505	HTTP Version Not Supported
