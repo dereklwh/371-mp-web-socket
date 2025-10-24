c028d702ea7d45c52f2127a7af4b775abafa4e3e
AgentQ32 <144646762+AgentQ32@users.noreply.github.com>
2025-10-24 14:07:13 -0700

    Update server.py

--- server.py
+++ server.py
@@ -18,21 +18,46 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind((HOST, PORT))
 serverSocket.listen(5)
 
-print ('The server is ready to receive')
+print (f'Listening on port {PORT}...')
 
 while True:
     # Server waits on client connections
     clientSocket, clientAddr = serverSocket.accept()
 
     # Get client request
-    clientRequest =clientSocket.recv(1024).decode()
-    print(clientRequest)
+    clientRequest = clientSocket.recv(1024).decode()
+    #print(clientRequest)
 
-    # Send HTTP response
-    serverResponse = 'HTTP/1.1 200 OK\n\nHello World'
-    print(serverResponse)
+    # Splits headers by line 
+    headers = clientRequest.split('\n')
+
+    # Splits the items in the first header line
+    headerComponents = headers[0].split(' ')
+
+    # Takes the path component of the header line
+    path = headerComponents[1]
+    
+    # Open and display file
+    if path == '/test.html':
+        fin = open('test.html')
+        content = fin.read()
+        fin.close()
+
+        # Send HTTP response 200 OK
+        serverResponse = 'HTTP/1.1 200 OK\n\n' + content
+
+        print(serverResponse)
+    
+    if path == '/garbage.txt':
+        
+        # Send HTTP response 404 Not Found
+        serverResponse = 'HTTP/1.1 404 Not Found\n\n'
+
+        print(serverResponse)
+
     clientSocket.sendall(serverResponse.encode())
     clientSocket.close()
+    
 
 # Close server socket
 serverSocket.close()
