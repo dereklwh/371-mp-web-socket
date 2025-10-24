a6fee53eafb82d52bb5e7f74ded33ce580312c85
dereklwh <derek141772@gmail.com>
2025-10-24 12:37:06 -0700

    add some helper functions and 505 response logic

--- server.py
+++ server.py
@@ -13,6 +13,14 @@ from socket import *
 HOST = '127.0.0.1' # Loopback address (localhost)
 PORT = 12000
 
+STATUS_TEXT = {
+    200: "OK",
+    304: "Not Modified",
+    403: "Forbidden",
+    404: "Not Found",
+    505: "HTTP Version Not Supported"
+}
+
 # Socket creation
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind((HOST, PORT))
@@ -20,6 +28,21 @@ serverSocket.listen(5)
 
 print ('The server is ready to receive')
 
+# helper function to get HTTP request line
+def get_request_line(request):
+    lines = request.split('\r\n')
+    if len(lines) > 0:
+        return lines[0]
+    return ''
+
+def get_html_version(request):
+    request_line = get_request_line(request)
+    parts = request_line.split(' ')
+    if len(parts) == 3:
+        return parts[2]
+    return ''
+
+# assume we are using http version 1.1
 while True:
     # Server waits on client connections
     clientSocket, clientAddr = serverSocket.accept()
@@ -27,6 +50,16 @@ while True:
     # Get client request
     clientRequest =clientSocket.recv(1024).decode()
     print(clientRequest)
+    print(get_request_line(clientRequest))
+    html_version = get_html_version(clientRequest)
+
+    # HTTP 505 response
+    if html_version != 'HTTP/1.0':
+        serverResponse = 'HTTP/1.1 505 HTTP Version Not Supported\n\n505 HTTP Version Not Supported'
+        print(serverResponse)
+        clientSocket.sendall(serverResponse.encode())
+        clientSocket.close()
+        continue
 
     # Send HTTP response
     serverResponse = 'HTTP/1.1 200 OK\n\nHello World'
