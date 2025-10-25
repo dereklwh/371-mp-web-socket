8d2f73a1b0f2a76c61e14edd6740d960c69f9f65
dereklwh <derek141772@gmail.com>
2025-10-24 19:22:50 -0700

    refactor and add comments

--- server.py
+++ server.py
@@ -21,13 +21,6 @@ STATUS_TEXT = {
     505: "HTTP Version Not Supported"
 }
 
-# Socket creation
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind((HOST, PORT))
-serverSocket.listen(5)
-
-print ('The server is ready to receive')
-
 # helper function to get HTTP request line
 def get_request_line(request):
     lines = request.split('\r\n')
@@ -35,12 +28,20 @@ def get_request_line(request):
         return lines[0]
     return ''
 
+# helper function to get HTTP version from request line
 def get_html_version(request):
     request_line = get_request_line(request)
     parts = request_line.split(' ')
     if len(parts) == 3:
         return parts[2]
     return ''
+
+# Socket creation (TCP)
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind((HOST, PORT))
+serverSocket.listen(5)
+
+print ('The server is ready to receive')
 
 # assume we are using http version 1.1
 while True:
@@ -54,18 +55,20 @@ while True:
     html_version = get_html_version(clientRequest)
 
     # HTTP 505 response
-    if html_version != 'HTTP/1.0':
-        serverResponse = 'HTTP/1.1 505 HTTP Version Not Supported\n\n505 HTTP Version Not Supported'
+    # To test, use curl the following command: 
+    # curl --http1.0 http://127.0.0.1:12000/
+    if html_version != 'HTTP/1.1':
+        serverResponse = 'HTTP/1.1 505 {}\n\n'.format(STATUS_TEXT[505])
         print(serverResponse)
         clientSocket.sendall(serverResponse.encode())
         clientSocket.close()
         continue
-
-    # Send HTTP response
-    serverResponse = 'HTTP/1.1 200 OK\n\nHello World'
-    print(serverResponse)
-    clientSocket.sendall(serverResponse.encode())
-    clientSocket.close()
+    else:
+        # Send HTTP response
+        serverResponse = 'HTTP/1.1 200 OK\n\nHello World'
+        print(serverResponse)
+        clientSocket.sendall(serverResponse.encode())
+        clientSocket.close()
 
 # Close server socket
 serverSocket.close()
