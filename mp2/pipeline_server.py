d014ff9571ecaf4f75b0baae84c2a212b4e3a868
dereklwh <derek141772@gmail.com>
2025-11-21 09:12:46 -0800

    simplified UDP server

--- mp2/pipeline_server.py
+++ mp2/pipeline_server.py
@@ -1,30 +1,13 @@
-# Include Python's Socket Library
 from socket import *
 
-# Specify Server Port
 serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
 
-# Create TCP welcoming socket
-serverSocket = socket(AF_INET,SOCK_STREAM)
+print('Server ready')
 
-# Bind the server port to the socket
-serverSocket.bind(('',serverPort))
-
-# Server begins listening for incoming TCP connections
-serverSocket.listen(1)
-print ('The server is ready to receive')
-
-while True: # Loop forever
-     # Server waits on accept for incoming requests.
-     # New socket created on return
-     connectionSocket, addr = serverSocket.accept()
-     
-     # Read from socket (but not address as in UDP)
-     sentence = connectionSocket.recv(1024).decode()
-     
-     # Send the reply
-     capitalizedSentence = sentence.upper()
-     connectionSocket.send(capitalizedSentence.encode())
-     
-     # Close connection to client (but not welcoming socket)
-     connectionSocket.close()
+while True:
+    data, addr = serverSocket.recvfrom(2048)  # receive packet + client address
+    sentence = data.decode()
+    capitalized = sentence.upper().encode()
+    serverSocket.sendto(capitalized, addr)
