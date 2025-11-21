dd9279c10fc8f2798899d3e39e797d2c414c4c35
dereklwh <derek141772@gmail.com>
2025-11-21 11:02:46 -0800

    wip

--- mp2/client.py
+++ mp2/client.py
@@ -1,8 +1,18 @@
 from socket import *
 
+# helper function to make custom packet
+def make_packet(seq, ack, rwnd, flags, payload: bytes) -> bytes:
+    header = f"{seq}|{ack}|{rwnd}|{flags}|".encode()
+    return header + payload
+
+# Client is used to send a packet to the server and receive a response
 client = socket(AF_INET, SOCK_DGRAM)
 server_addr = ('127.0.0.1', 12000)
 
-client.sendto(b'hello server', server_addr)
+# need to implement 3 way handshake:
+# then simulate sending multiple packets with payload in accordance to rwnd/cwnd
+# client handles cwnd
+pkt = make_packet(seq=0, ack=0, rwnd=32, flags="SYN", payload=b"hi my name is derekkk")
+client.sendto(pkt, server_addr)
 data, addr = client.recvfrom(2048)
 print("Server replied:", data.decode())
--- mp2/pipeline_server.py
+++ mp2/pipeline_server.py
@@ -1,15 +1,21 @@
 from socket import *
 
-serverPort = 12000
+HOST = '127.0.0.1'
+PORT = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
+serverSocket.bind((HOST, PORT))
 
 print('Server ready')
 
+def parse_packet():
+    pass
+
+# server handles rwnd
 while True:
     data, addr = serverSocket.recvfrom(2048)  # receive packet + client address
     sentence = data.decode()
     capitalized = sentence.upper().encode()
     serverSocket.sendto(capitalized, addr)
     print(f"Processed request from {addr}")
-    
+    print(f"data: {data}")
+
